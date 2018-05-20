9e7f59000fd8b012e6f724d80914071232fa2d28
Nick Anderson <nick@cmdln.org>
2018-05-20 13:19:29 -0500

    fixup

--- deploy.sh
+++ deploy.sh
@@ -1,89 +1,38 @@
-#!/bin/bash
-[[ $@ == *--quiet* ]]
-QUIET=$?
+#!/usr/bin/env bash
 
-function output {
-    if [[ ! $QUIET -eq 0 ]]; then
-        echo $@
-    fi
+# This script allows you to easily and quickly generate and deploy your website
+# using Hugo to your personal GitHub Pages repository. This script requires a
+# certain configuration, run the `setup.sh` script to configure this. See
+# https://hjdskes.github.io/blog/deploying-hugo-on-personal-github-pages/index.html
+# for more information.
+
+# Set the English locale for the `date` command.
+export LC_TIME=en_US.UTF-8
+
+# GitHub username.
+USERNAME=nickanderson
+# Name of the branch containing the Hugo source files.
+SOURCE=src
+# The commit message.
+MESSAGE="Site rebuild $(date)"
+
+msg() {
+    printf "\033[1;32m :: %s\n\033[0m" "$1"
 }
 
-function echoerr {
-    echo >&2 $@
-}
+msg "Pulling down the \`master\` branch into \`public\` to help avoid merge conflicts"
+git subtree pull --prefix=public --rebase \
+	git@github.com:$USERNAME/$USERNAME.github.io.git master -m "Merge master"
 
-set -o errexit
+msg "Building the website"
+hugo
 
-VERSION="1.0.1"
+msg "Pushing the updated \`public\` folder to the \`$SOURCE\` branch"
+git add public
+git commit -m "$MESSAGE"
+git push origin "$SOURCE"
 
-if ! `git diff --exit-code > /dev/null`; then
-    echoerr "Uncommitted changes"
-    echoerr "Please commit your changes before you deploy."
-    exit 1
-fi
-
-THEMES_DIR=$(hugo config | grep "themesdir" | egrep '".+"' -o)
-THEMES_DIR=${THEMES_DIR:1:${#THEMES_DIR}-2}
-
-TARGET_BRANCH=$(hugo config | grep "publishbranch" | egrep '".+"' -o)
-TARGET_BRANCH=${TARGET_BRANCH:1:${#TARGET_BRANCH}-2}
-
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-CURRENT_COMMIT=$(git rev-parse --short HEAD)
-
-output "$CURRENT_BRANCH@$CURRENT_COMMIT => $TARGET_BRANCH"
-
-if ! `git rev-parse --verify --quiet "$TARGET_BRANCH" > /dev/null`; then
-    output -e "\nThe branch '$TARGET_BRANCH' doesn't exits."
-    output -n "Creating it..."
-    git checkout --orphan "$TARGET_BRANCH" --quiet
-    git reset --hard --quiet &> /dev/null
-    git commit --allow-empty -m "Initial commit" --quiet
-    git checkout "$CURRENT_COMMIT" --quiet &> /dev/null
-    output 'Done.'
-fi
-
-function get_temp_dir {
-    # cannot use mktemp because hugo on windows doesn't work with path
-    # starting with /
-    local temp="../"
-    while [[ -d $temp ]]; do
-        local temp="../hugo-build-$RANDOM"
-    done
-    mkdir $temp
-    echo $temp
-}
-
-TEMP=$(get_temp_dir)
-hugo -d "$TEMP" --quiet
-
-git checkout "$TARGET_BRANCH" --quiet &> /dev/null
-
-find -not -path "*.git*" -not -path "*$THEMES_DIR*" -type f -delete
-find -depth -not -path "*.git*" -not -path "*$THEMES_DIR*" -type d -empty -exec rmdir {} +
-
-mv $TEMP/* .
-
-rmdir $TEMP
-
-git add . &> /dev/null
-
-if [[ -d "$THEMES_DIR" ]]; then
-  git rm --cached "$THEMES_DIR" -r --quiet
-fi
-
-if `git diff --exit-code --cached > /dev/null`; then
-    output "No change since last build"
-    output "-> don't commit or push"
-else
-    output "Commit..."
-    git commit -m "Auto build for $CURRENT_COMMIT" -m "deployer version: $VERSION" --quiet
-
-    output "Push..."
-    git push origin HEAD --quiet
-fi
-
-git checkout "$CURRENT_BRANCH" --quiet
-
+msg "Pushing the updated \`public\` folder to the \`master\` branch"
+git subtree push --prefix=public \
+	git@github.com:$USERNAME/$USERNAME.github.io.git master
 
